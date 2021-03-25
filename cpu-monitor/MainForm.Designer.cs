40565643a1ab430234e216941ca11323e28a365f
Iaroslav <IaroslavInternational@yandex.ru>
2021-03-26 02:03:38 +0300

    Добавлено меню и улучшена система отрисовки графика.

---
+++ cpu-monitor/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace cpu_monitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ExitBtn = new FontAwesome.Sharp.IconButton();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.MainChart = new LiveCharts.Wpf.CartesianChart();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(193)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.iconPictureBox1);
+            this.panel1.Controls.Add(this.ExitBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 36);
+            this.panel1.TabIndex = 1;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ExitBtn.FlatAppearance.BorderSize = 0;
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.IconChar = FontAwesome.Sharp.IconChar.SignOutAlt;
+            this.ExitBtn.IconColor = System.Drawing.Color.Black;
+            this.ExitBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.ExitBtn.IconSize = 36;
+            this.ExitBtn.Location = new System.Drawing.Point(260, 0);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(40, 36);
+            this.ExitBtn.TabIndex = 0;
+            this.ExitBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(2)))), ((int)(((byte)(30)))));
+            this.panel2.Controls.Add(this.elementHost1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 304);
+            this.panel2.TabIndex = 0;
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.elementHost1.Location = new System.Drawing.Point(3, 3);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(294, 298);
+            this.elementHost1.TabIndex = 1;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.MainChart;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(193)))), ((int)(((byte)(0)))));
+            this.iconPictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.LaptopCode;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 36;
+            this.iconPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(36, 36);
+            this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CPU-monitor";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 475);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart MainChart;
+        private FontAwesome.Sharp.IconButton ExitBtn;
+        private System.Windows.Forms.Label label1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+    }
+}
+
---
+++ cpu-monitor/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+
+using FontAwesome;
+using FontAwesome.Sharp;
+using System.Windows.Media;
+
+namespace cpu_monitor
+{
+    public partial class MainForm : Form
+    {
+        private PerformanceCounter performanceCounter = new PerformanceCounter(
+            "Processor Information", "% Processor Time", "_Total");
+
+        private int AllowedPts = 30;
+        private int SecondsGone = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width,
+                Screen.PrimaryScreen.Bounds.Height - this.Height - 40);
+
+            TopMost = true;
+
+            var gradientBrush = new LinearGradientBrush
+            {
+                StartPoint = new System.Windows.Point(0, 0),
+                EndPoint = new System.Windows.Point(0, 1)
+            };
+
+            gradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromRgb(122, 42, 241), 0));
+            gradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromRgb(0, 255, 111), 1));
+
+            MainChart.Series.Add(new LineSeries
+            {
+                Values = new ChartValues<float>(),
+                ScalesYAt = 0,
+                PointGeometry = null,
+                Fill = gradientBrush,
+                StrokeThickness = 1,
+                LabelPoint = point => point.Y + " " + "%"
+            });
+
+            MainChart.AxisX.Add(new Axis
+            {
+                Visibility = System.Windows.Visibility.Hidden
+            });
+
+            MainChart.AxisY.Add(new Axis
+            {
+                MinValue = 0.0f,
+                MaxValue = 100.0f
+            });
+        }
+
+        private void AddValToChart(float val)
+        {
+            if (MainChart.Series[0].Values.Count == AllowedPts)
+            {
+                MainChart.Series[0].Values.RemoveAt(0);
+            }
+
+            MainChart.Series[0].Values.Add(val);
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            AddValToChart(performanceCounter.NextValue());
+            SecondsGone++;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MainTimer.Start();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            if (MainTimer.Enabled)
+            {
+                MainTimer.Stop();
+            }
+
+            this.Close();
+        }
+    }
+}
